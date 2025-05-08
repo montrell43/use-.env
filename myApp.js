eee04d7dff0a32d617c07d9f1a9593e7e32ab96b
montrell43 <tmerriweather@persevernow.org>
2025-05-08 13:03:01 -0500

    Add commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
+
+
 app.get('/json', (req, res) => {
   res.json({ message: 'Hello json' });
 });
