2437a01287e779712e31bbdbc306cbd5e9cc1e7c
montrell43 <tmerriweather@persevernow.org>
2025-05-08 13:56:42 -0500

    Add commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
-
-
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
+});
+
 
 app.get('/json', (req, res) => {
   let message = 'Hello json';
@@ -13,6 +22,5 @@ app.get('/json', (req, res) => {
 
   res.json({ message });
 });
-
 
 module.exports = app;
