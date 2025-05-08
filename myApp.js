c55a42877c7978a8edd62d04e3cacc9edbd2ce4f
montrell43 <tmerriweather@persevernow.org>
2025-05-08 12:54:09 -0500

    Add commit

--- myApp.js
+++ myApp.js
@@ -1,33 +1,8 @@
-// Serve JSON on a Specific Route
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-  });
-
-
-app.get("/json", (req, res) => {
-    res.json({
-        "message": "Hello json"
-    });
-});
 app.get('/json', (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
-  }
-  res.json({ message });
+  res.json({ message: 'Hello json' });
 });
 
-app.get("/json", (req, res) => {
-    let message;
-    if (process.env['MESSAGE_STYLE'] === "uppercase") {
-        message = "Hello json".toUpperCase();
-    }
-    else {
-        message = "Hello json";
-    }
-    res.json({ "message": `${message}` });
-});
+module.exports = app;
